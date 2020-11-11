f11d854d85279f588c83c084066121571d2c78fa
astradotin <60748160+astradotin@users.noreply.github.com>
2020-11-11 20:41:53 +0530

    agecalc.py

--- agecalc.py
+++ agecalc.py
@@ -1,17 +1,58 @@
 import datetime
 from tkinter import *
 from tkinter import messagebox
+import sqlite3
 
+DB=sqlite3.connect('StoreThisPlease.sqlite3')
+cursor=DB.cursor()
+INDEX,E1,E2=0,None,None
+query1='''
+create table if not exists DOBS(
+    id integer,
+    birthday varchar(30),
+    age varchar(30)
+);
+'''
+query2='''
+select count(*) from DOBS;
+'''
+cursor.execute(query1)
+INDEX=tuple(cursor.execute(query2))[0][0]+1
+
+def showLogs():
+    global INDEX
+    store=list(cursor.execute('select * from DOBS'))
+    print(store)
+    Display=Toplevel()
+    Vbar=Scrollbar(Display)
+    box=Listbox(Display,yscrollcommand=Vbar.set,width=50)
+    Vbar.config(orient=VERTICAL,command=box.yview)
+    box.insert(END,str(['Date of Birth',' Age ']))
+    for i in store:
+        i=list(i)
+        del i[0]
+        box.insert(END,str(i))
+    Vbar.pack(side=RIGHT,fill=Y)
+    box.pack(expand=True)
 def setFullAge(y,m,d):
+    global E2,E1,INDEX,DB
     fullDay.set(str(d))
     fullMonth.set(str(m))
     fullYear.set(str(y))
+    if d!=0 and m!=0 and y!=0:
+        E2=(str(y)+'y'+str(m)+'m'+str(d)+'d')
+        cursor.execute('Insert into DOBS values ({},"{}","{}")'.format(INDEX,E1,E2))
+        DB.commit()
+        INDEX+=1
+        E1,E2=None,None
 
 def calculateAge():
+    global E1
     currentdate = datetime.datetime.now()
     birthday = inputfield.get()
     try:
         birthdayDate=datetime.datetime.strptime(birthday,'%d-%m-%Y')
+        E1=(birthday)
     except:
         setFullAge(0,0,0)
         messagebox.showerror("Error","Wrong Value of Birthday")
@@ -34,6 +75,11 @@ HEIGHT = 350
 WIDTH = 600
 
 root = Tk()
+def on_closing():
+    global DB
+    DB.close()
+    root.destroy()
+root.protocol("WM_DELETE_WINDOW", on_closing)
 root.title("Age Calculator")
 
 canvas = Canvas(root,width = WIDTH, height = HEIGHT)
@@ -41,6 +87,8 @@ canvas.pack()
 
 frame = Frame(root,bg="#ececec")
 frame.place(relheight=1,relwidth=1,relx=0,rely=0)
+history=Button(frame,command=lambda :showLogs(),text=' HISTORY ')
+history.pack(side=RIGHT)
 
 headingLabel = Label(frame,text="Y O U R . D E T A I L S",bg="#ececec",fg="#000000",font='DecoType 24')
 headingLabel.pack(fill = X, expand =False, ipady = 18)
@@ -84,11 +132,9 @@ fullDayText.place(relheight=.05,relwidth=.11,relx=.76,rely=.70)
 
 setFullAge(0,0,0)
 
-
 copyrightText = Label(frame,text = "Developed By Nishant",bg="#ececec",fg="#000000",font='AppleGothic 12')
 copyrightText2=Label(frame,text="Teacher's Name : V. Devendran, Department : CSE, 2020",bg="#ececec",fg="#000000",font='AppleGothic 12')
 copyrightText.place(relx=.09,rely=.85,relwidth=.8,relheight=.1)
 copyrightText2.place(relx=.09,rely=.93,relwidth=.8,relheight=.05)
-
 
 root.mainloop()
